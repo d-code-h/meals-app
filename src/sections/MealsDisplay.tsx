bb507e45013cb668aace072b8527698dd92b433f
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2024-10-12 04:12:44 +0100

    Extract meals into a component and UI fix

--- src/sections/MealsDisplay.tsx
+++ src/sections/MealsDisplay.tsx
@@ -2,13 +2,11 @@ import { useContext } from 'react';
 import { MealsContext } from '../context/MealsContext';
 import { MealsContextType } from '../lib/types';
 
-import { MdOutlineFavorite, MdOutlineFavoriteBorder } from 'react-icons/md';
 import clsx from 'clsx';
+import Meals from '../components/Meals';
 
 const MealsDisplay = () => {
-  const { meals, favorites, handleFavorites, search } = useContext(
-    MealsContext
-  ) as MealsContextType;
+  const { meals, search } = useContext(MealsContext) as MealsContextType;
 
   const filteredMeals = meals.filter((meal) =>
     Object.values(meal).join('').toLowerCase().includes(search.toLowerCase())
@@ -37,44 +35,17 @@ const MealsDisplay = () => {
                 strIngredient2,
                 strIngredient3,
               }) => (
-                <section
+                <Meals
                   key={idMeal}
-                  className="flex flex-col gap-3 my-3 pb-1 justify-center bg-white rounded-md"
-                >
-                  <img
-                    src={strMealThumb}
-                    alt={strMeal}
-                    width={300}
-                    height={300}
-                    className="w-full h-[400px] sm:h-[300px] md:auto rounded-md"
-                  />
-                  <div className="p-3">
-                    <div className="flex justify-between mb-4">
-                      <h4 className="text-xl font-semibold">{strMeal}</h4>
-                      <button
-                        type="button"
-                        onClick={() => handleFavorites(idMeal)}
-                      >
-                        {favorites.includes(idMeal) ? (
-                          <MdOutlineFavorite className="fill-red-500" />
-                        ) : (
-                          <MdOutlineFavoriteBorder className="fill-blue-500" />
-                        )}
-                      </button>
-                    </div>
-                    <div className="flex gap-3 flex-wrap justify-center w-[300px] mx-auto">
-                      <small className="bg-red-200 px-2 py-1 rounded-md">
-                        {strIngredient1}
-                      </small>
-                      <small className="bg-green-200 px-2 py-1 rounded-md">
-                        {strIngredient2}
-                      </small>
-                      <small className="bg-yellow-200 px-2 py-1 rounded-md">
-                        {strIngredient3}
-                      </small>
-                    </div>
-                  </div>
-                </section>
+                  idMeal={idMeal}
+                  strMeal={strMeal}
+                  strMealThumb={strMealThumb}
+                  strIngredient={[
+                    strIngredient1,
+                    strIngredient2,
+                    strIngredient3,
+                  ]}
+                />
               )
             )}
         </div>
